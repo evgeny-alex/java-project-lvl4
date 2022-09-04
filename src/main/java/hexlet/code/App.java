76dcad2b087f2692f11891c81a707b4d0ccbd3cd
evgeny-alex <e.aleksandrov1@alumni.nsu.ru>
2022-09-04 12:27:47 +0300

    Init commit

---
+++ src/main/java/hexlet/code/App.java
@@ -0,0 +1,65 @@
+package hexlet.code;
+
+import hexlet.code.controllers.RootController;
+import io.javalin.Javalin;
+import io.javalin.plugin.rendering.template.JavalinThymeleaf;
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+public final class App {
+
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "3000");
+        return Integer.valueOf(port);
+    }
+
+    // Javalin поддерживает работу с шаблонизатором thymeleaf
+    private static TemplateEngine getTemplateEngine() {
+        // Создаём инстанс движка шаблонизатора
+        TemplateEngine templateEngine = new TemplateEngine();
+        // Добавляем к нему диалекты
+        templateEngine.addDialect(new LayoutDialect());
+        templateEngine.addDialect(new Java8TimeDialect());
+        // Настраиваем преобразователь шаблонов, так, чтобы обрабатывались
+        // шаблоны в директории /templates/
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("/templates/");
+        // Добавляем преобразователь шаблонов к движку шаблонизатора
+        templateEngine.addTemplateResolver(templateResolver);
+
+        return templateEngine;
+    }
+
+    // Метод добавляет маршруты в переданное приложение
+    private static void addRoutes(Javalin app) {
+        // Для GET-запроса на маршрут / будет выполняться
+        // обработчик welcome в контроллере RootController
+        app.get("/", RootController.welcome);
+    }
+
+
+    public static Javalin getApp() {
+        Javalin app = Javalin.create(config -> {
+            config.enableDevLogging();
+
+            config.enableWebjars();
+            JavalinThymeleaf.configure(getTemplateEngine());
+        });
+
+        addRoutes(app);
+
+        app.before(ctx -> {
+            ctx.attribute("ctx", ctx);
+        });
+
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(getPort());
+    }
+}
+

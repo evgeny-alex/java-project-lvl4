637a2c7071fc134b9db826f2e467a460c7eeab06
evgeny-alex <e.aleksandrov1@alumni.nsu.ru>
2022-09-10 11:05:59 +0300

    Добавление создания и проверки

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -17,55 +17,54 @@ public final class App {
 
     private static int getPort() {
         String port = System.getenv().getOrDefault("PORT", "3000");
-        return Integer.valueOf(port);
+        return Integer.parseInt(port);
     }
 
-    // Javalin поддерживает работу с шаблонизатором thymeleaf
+    private static String getMode() {
+        return System.getenv().getOrDefault("APP_ENV", "development");
+    }
+
+    private static boolean isProduction() {
+        return getMode().equals("production");
+    }
+
     private static TemplateEngine getTemplateEngine() {
-        // Создаём инстанс движка шаблонизатора
         TemplateEngine templateEngine = new TemplateEngine();
-        // Добавляем к нему диалекты
-        templateEngine.addDialect(new LayoutDialect());
-        templateEngine.addDialect(new Java8TimeDialect());
-        // Настраиваем преобразователь шаблонов, так, чтобы обрабатывались
-        // шаблоны в директории /templates/
+
         ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
         templateResolver.setPrefix("/templates/");
-        // Добавляем преобразователь шаблонов к движку шаблонизатора
+
         templateEngine.addTemplateResolver(templateResolver);
+        templateEngine.addDialect(new LayoutDialect());
+        templateEngine.addDialect(new Java8TimeDialect());
 
         return templateEngine;
     }
 
-    // Метод добавляет маршруты в переданное приложение
     private static void addRoutes(Javalin app) {
-        // Для GET-запроса на маршрут / будет выполняться
-        // обработчик welcome в контроллере RootController
         app.get("/", RootController.getWelcome());
 
-        app.routes(() -> {
-            path("urls", () -> {
-                get(UrlController.listUrls);
-                post(UrlController.createUrl);
-            });
-        });
+        app.get("/urls", UrlController.listUrls);
 
+        app.post("/urls",  UrlController.createUrl);
+
+        app.get("/urls/{id}", UrlController.showUrl);
+
+        app.post("/urls/{id}/checks", UrlController.checkUrl);
     }
-
 
     public static Javalin getApp() {
         Javalin app = Javalin.create(config -> {
-            config.enableDevLogging();
-
+            if (!isProduction()) {
+                config.enableDevLogging();
+            }
             config.enableWebjars();
             JavalinThymeleaf.configure(getTemplateEngine());
         });
 
         addRoutes(app);
 
-        app.before(ctx -> {
-            ctx.attribute("ctx", ctx);
-        });
+        app.before(ctx -> ctx.attribute("ctx", ctx));
 
         return app;
     }
@@ -75,4 +74,5 @@ public final class App {
         app.start(getPort());
     }
 }
+
 
--- src/main/java/hexlet/code/controllers/UrlController.java
+++ src/main/java/hexlet/code/controllers/UrlController.java
@@ -1,17 +1,25 @@
 package hexlet.code.controllers;
 
-import hexlet.code.domain.Url;
-import hexlet.code.domain.UrlCheck;
-import hexlet.code.domain.query.QUrl;
-import hexlet.code.domain.query.QUrlCheck;
-import io.ebean.PagedList;
-import io.javalin.http.Handler;
 
-import java.net.URL;
-import java.util.List;
+import io.javalin.http.Handler;
+import io.javalin.http.NotFoundResponse;
+import io.ebean.PagedList;
 import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.List;
 import java.util.stream.IntStream;
+import java.util.stream.Collectors;
+import java.net.URL;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import kong.unirest.Unirest;
+import kong.unirest.HttpResponse;
+
+import hexlet.code.domain.query.QUrl;
+import hexlet.code.domain.Url;
+import hexlet.code.domain.query.QUrlCheck;
+import hexlet.code.domain.UrlCheck;
+
 
 public final class UrlController {
 
@@ -98,6 +106,72 @@ public final class UrlController {
         ctx.attribute("currentPage", currentPage);
         ctx.render("urls/index.html");
     };
+
+
+    /**
+     * Метод обрабатывает GET-запрос по получению информации по конкретному сайту
+     *
+     * id - идентификатор сайта в БД
+     */
+    public static Handler showUrl = ctx -> {
+        int id = ctx.pathParamAsClass("id", Integer.class).getOrDefault(null);
+
+        Url url = new QUrl()
+                .id.equalTo(id)
+                .urlChecks.fetch()
+                .orderBy()
+                .urlChecks.createdAt.desc()
+                .findOne();
+
+        if (url == null) {
+            throw new NotFoundResponse();
+        }
+
+        ctx.attribute("url", url);
+        ctx.render("urls/show.html");
+    };
+
+    /**
+     * Метод обрабатывает POST-запрос по проверке сайта
+     *
+     * id - идентификатор сайта в БД
+     */
+    public static Handler checkUrl = ctx -> {
+        int id = ctx.pathParamAsClass("id", Integer.class).getOrDefault(null);
+
+        Url url = new QUrl()
+                .id.equalTo(id)
+                .findOne();
+
+        if (url == null) {
+            throw new NotFoundResponse();
+        }
+
+        try {
+            HttpResponse<String> response = Unirest.get(url.getName()).asString();
+            Document doc = Jsoup.parse(response.getBody());
+
+            int statusCode = response.getStatus();
+            String title = doc.title();
+            Element h1Element = doc.selectFirst("h1");
+            String h1 = h1Element == null ? "" : h1Element.text();
+            Element descriptionElement = doc.selectFirst("meta[name=description]");
+            String description = descriptionElement == null ? "" : descriptionElement.attr("content");
+
+            UrlCheck newUrlCheck = new UrlCheck(statusCode, title, h1, description);
+            url.getUrlChecks().add(newUrlCheck);
+            url.save();
+
+            ctx.sessionAttribute("flash", "Страница успешно проверена");
+            ctx.sessionAttribute("flash-type", "success");
+        } catch (Exception e) {
+            ctx.sessionAttribute("flash", e.getMessage());
+            ctx.sessionAttribute("flash-type", "danger");
+        }
+
+        ctx.redirect("/urls/" + url.getId());
+    };
+
 
 
 }
